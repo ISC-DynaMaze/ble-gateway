62daa74374251dab27eadbcf1fd344b802de85a5
LordBaryhobal <lordbaryhobal@gmail.com>
2026-04-27 10:27:19 +0200

    feat: add method to set led color

--- ble_gateway.py
+++ ble_gateway.py
@@ -1,7 +1,10 @@
 import asyncio
 import logging
+import struct
+from functools import partial
 
 from bleak import BleakClient, BleakScanner
+from bleak.backends.characteristic import BleakGATTCharacteristic
 from bleak.backends.device import BLEDevice
 
 
@@ -21,29 +24,44 @@ class BLEGateway:
             timeout=timeout, service_uuids=[self.SERVICES_UUID]
         )
         return devices
-    
+
     async def connect(self, device: BLEDevice):
         self.logger.info(f"Connecting to {device.address}")
         async with BleakClient(device, self.on_disconnected) as client:
-            await client.start_notify(self.BUTTON_CHAR, self.on_button)
-            await client.start_notify(self.IR_CHAR, self.on_ir)
+            await client.start_notify(self.BUTTON_CHAR, partial(self.on_button, client))
+            await client.start_notify(self.IR_CHAR, partial(self.on_ir, client))
             self.clients[client.address] = client
             await asyncio.sleep(1e9)
-    
+
     def on_disconnected(self, client: BleakClient):
         self.logger.info(f"Client {client.address} disconnected")
-    
-    def on_button(self, _, data: bytes):
-        self.logger.info(f"Received button update: {data}")
-    
-    def on_ir(self, _, data: bytes):
-        self.logger.info(f"Received IR update: {data}")
+        self.clients.pop(client.address)
+
+    def on_button(
+        self, client: BleakClient, char: BleakGATTCharacteristic, data: bytes
+    ) -> None:
+        self.logger.info(f"Received button update from {client.address}: {data}")
+
+    def on_ir(
+        self, client: BleakClient, char: BleakGATTCharacteristic, data: bytes
+    ) -> None:
+        self.logger.info(f"Received IR update from {client.address}: {data}")
+        occupied: bool = struct.unpack(">B", data)[0] != 0
+        if occupied:
+            asyncio.create_task(self.set_led(client.address, (255, 0, 0)))
+        else:
+            asyncio.create_task(self.set_led(client.address, (0, 255, 0)))
+
+    async def set_led(self, address: str, color: tuple[int, int, int]) -> bool:
+        client = self.clients.get(address)
+        if client is None:
+            self.logger.error(f"Could not find client {address}")
+            return False
+
+        payload: bytes = struct.pack(">BBB", *color)
+        await client.write_gatt_char(self.LED_CHAR, payload)
+        return True
 
     async def run(self):
         devices: list[BLEDevice] = await self.scan()
-        await asyncio.gather(
-            *(
-                self.connect(device)
-                for device in devices
-            )
-        )
+        await asyncio.gather(*(self.connect(device) for device in devices))
