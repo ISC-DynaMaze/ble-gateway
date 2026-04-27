69c3a64401f668f366ac491b08c623ee37bbf22e
LordBaryhobal <lordbaryhobal@gmail.com>
2026-04-27 15:04:09 +0200

    feat: link BLE and MQTT gateways

--- main.py
+++ main.py
@@ -1,17 +1,66 @@
 import asyncio
+import json
 import logging
+import os
+import struct
+
+from bleak import BleakClient
+from bleak.backends.characteristic import BleakGATTCharacteristic
+from paho.mqtt.client import MQTTMessage
 
 from ble_gateway import BLEGateway
 from mqtt_gateway import MQTTGateway
 
 
-def main():
+async def main():
     logging.basicConfig(level=logging.INFO)
-    ble = BLEGateway()
-    mqtt = MQTTGateway()
+    logger = logging.getLogger("MainLogger")
 
-    asyncio.run(ble.run())
+    loop = asyncio.get_running_loop()
+
+    mqtt_host = os.environ.get("MQTT_HOST", "isc-coordinator.lan")
+    mqtt_port = int(os.environ.get("MQTT_PORT", 1883))
+
+    ble = BLEGateway()
+    mqtt = MQTTGateway(mqtt_host, mqtt_port)
+
+    def on_button(client: BleakClient, char: BleakGATTCharacteristic, data: bytes):
+        is_pressed: bool = struct.unpack(">B", data)[0] != 0
+        address: str = client.address
+        topic: str = f"{address}/button"
+        logger.info(f"{topic} -> {is_pressed}")
+        mqtt.send(topic, b"1" if is_pressed else b"0")
+    
+    def on_ir(client: BleakClient, char: BleakGATTCharacteristic, data: bytes):
+        is_occupied: bool = struct.unpack(">B", data)[0] != 0
+        address: str = client.address
+        topic: str = f"{address}/ir"
+        logger.info(f"{topic} -> {is_occupied}")
+        mqtt.send(topic, b"1" if is_occupied else b"0")
+    
+    def on_led(message: MQTTMessage):
+        uuid: str = message.topic.split("/")[0]
+        color: tuple[int, int, int]
+        try:
+            data = json.loads(message.payload)
+            assert isinstance(data, list)
+            assert len(data) == 3
+            assert all(map(lambda i: isinstance(i, int), data))
+            color = tuple(data)
+        except Exception as e:
+            logger.error(e)
+            return
+        logger.info(f"Setting led {uuid} to {color}")
+        loop.call_soon_threadsafe(asyncio.ensure_future, ble.set_led(uuid, color))
+
+    ble.on_button = on_button
+    ble.on_ir = on_ir
+
+    mqtt.add_listener("+/led", on_led)
+
+    mqtt.run()
+    await ble.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
